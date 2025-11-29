24c8630d28c22d71bf7b5d4991a9c9e8d5a16b74
Lucas Radovan <lucasradovan@gmail.com>
2025-11-29 10:49:25 -0800

    add slides for claiming that L_pad is in P

---
+++ L_pad_in_P.py
@@ -0,0 +1,57 @@
+from manim import *
+from manim_slides import Slide
+
+class LPadInP(Slide):
+    def construct(self):
+        myTemplate = TexTemplate()
+        myTemplate.add_to_preamble(r"\usepackage{mathrsfs}")
+
+        in_np = Tex(
+            r"Since the running time of ",
+            r"M' ", 
+            r"is polynomial, and it contains a non-deterministic simulation of running ",
+            r"M ",
+            r"on x, \underline{$\mathscr{L}_{\text{pad}} \in \mathcal{NP}$}.",
+            font_size=40,
+            tex_template=myTemplate
+        )
+        in_np[1].set_color(PINK)
+        in_np[3].set_color(YELLOW)
+        in_np.to_edge(UP, buff=1)
+
+        self.play(Write(in_np))
+
+        assumption = Tex(
+            r"We assume $\mathcal{P} = \mathcal{NP}$",
+            font_size=40,
+            tex_template=myTemplate
+        )
+        assumption.next_to(in_np, DOWN*4)
+
+        self.next_slide()
+
+        self.play(Write(assumption))
+
+        in_p = Tex(
+            r"Therefore, \underline{$\mathscr{L}_{\text{pad}} \in \mathcal{P}$",
+            font_size=40,
+            tex_template=myTemplate
+        )
+
+        in_p.next_to(assumption, DOWN*4)
+
+        self.next_slide()
+
+        self.play(Write(in_p))
+
+        l_in_exp = Tex(
+            r"We can use this to show that $\mathscr{L} \in \mathcal{EXP}$",
+            font_size=40,
+            tex_template=myTemplate
+        )
+
+        l_in_exp.next_to(in_p, DOWN*4)
+
+        self.next_slide()
+
+        self.play(Write(l_in_exp))
