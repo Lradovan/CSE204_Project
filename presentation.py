60557890bfc6a4352709f1f1613bf8b0ca26a381
Lucas Radovan <lucasradovan@gmail.com>
2025-12-02 09:34:51 -0800

    add presentation file to combine all slides

---
+++ presentation.py
@@ -0,0 +1,55 @@
+from manim import *
+from manim_slides import Slide
+
+# import all of the invididual slides
+from intro import Intro
+from assumption import Assumption
+from padding import Padding
+from deciding_L import DecidingL
+from deciding_L_pad import DecidingLPad
+from L_pad_in_P import LPadInP
+from tm_construction import TMConstruction
+from conclusion import Conclusion
+
+class Presentation(Slide):
+    def construct(self):
+
+        # (1) intro
+        Intro.construct(self)
+        self.next_slide() 
+        self.clear()
+
+        # (2) assumption
+        Assumption.construct(self)
+        self.next_slide() 
+        self.clear()
+
+        # (3) definition of L_pad
+        Padding.construct(self)
+        self.next_slide()
+        self.clear()
+
+        # (4) Decidability of L
+        DecidingL.construct(self)
+        self.next_slide()
+        self.clear()
+
+        # (5) Decidability of L_pad
+        DecidingLPad.construct(self)
+        self.next_slide()
+        self.clear()
+
+        # (6) L_pad is in P
+        LPadInP.construct(self)
+        self.next_slide()
+        self.clear()
+
+        # (7) TM Construction
+        TMConstruction.construct(self)
+        self.next_slide()
+        self.clear()
+
+        # (8) Conclusion
+        Conclusion.construct(self)
+        self.next_slide()
+        self.clear()
